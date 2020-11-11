dfad4d6b27706c009e596f8092f893e0550f628a
Maxx730 <max.kinghorn@gmail.com>
2020-11-11 11:15:19 -0500

    update

--- src/components/ReactImageScaler.js
+++ src/components/ReactImageScaler.js
@@ -13,7 +13,6 @@ export default class ReactImageScaler extends React.Component {
     this.scaleValueRef = createRef();
     this.rangeScaleRef = createRef();
     this.sourceRef = createRef();
-    this.previewRef = createRef();
 
     this.state = {
       canvasWidth: this.props.width ? this.props.width : window.innerWidth,
@@ -24,19 +23,22 @@ export default class ReactImageScaler extends React.Component {
   }
 
   componentDidMount() {
-    this.rangeScaleRef.current.value = 10;
-    this.canvas = this.canvasRef.current;
-    this.sourceCanvas = this.sourceRef.current;
-    this.context = this.canvas.getContext('2d');
+    if (this.props.src) {
+      this.rangeScaleRef.current.value = 10;
+      this.scaleValueRef.current.value = 1;
+      this.canvas = this.canvasRef.current;
+      this.sourceCanvas = this.sourceRef.current;
+      this.context = this.canvas.getContext('2d');
+    
+      this.image = new Image();
+      this.image.src = this.state.imageSource;
   
-    this.image = new Image();
-    this.image.src = this.state.imageSource;
-
-
-    //Initially loads the given image.
-    this.image.onload = () => {
-      this.drawSourceCanvas(1);
-      this.redrawCanvas(1);
+  
+      //Initially loads the given image.
+      this.image.onload = () => {
+        this.drawSourceCanvas(1);
+        this.redrawCanvas(1);
+      }
     }
   }
 
@@ -68,11 +70,7 @@ export default class ReactImageScaler extends React.Component {
           <input type='range' ref={this.rangeScaleRef} step={this.props.scaleStep ? this.props.scaleStep : '0.5'} min='1' max={this.props.maxScale ? this.props.maxScale * 10 : 30} onChange={this.scaleImage}/>
         </div>
         <div className='control-segment'>
-          <div className='control-scale-sizes'>
-            <button onClick={this.returnData}>
-              {this.props.buttonMessage ? this.props.buttonMessage : '2X'}
-            </button>
-          </div>
+          {this.renderScaleSizes()}
           <button onClick={this.returnData}>
             {this.props.buttonMessage ? this.props.buttonMessage : 'Apply'}
           </button>
@@ -93,11 +91,30 @@ export default class ReactImageScaler extends React.Component {
           </canvas>
           {this.renderControls()}
         </div>
-        <div className='react-scaler-preview'>
-          <img src = '' ref={this.previewRef}/>
-        </div>
       </>
     )
+  }
+
+  renderScaleSizes() {
+    return(
+      <div className='control-scale-sizes'>
+        <button onClick={() => {
+          this.redrawCanvas(0.5)
+        }}>
+            {this.props.buttonMessage ? this.props.buttonMessage : '0.5X'}
+        </button>
+        <button onClick={() => {
+          this.redrawCanvas(1)
+        }}>
+            {this.props.buttonMessage ? this.props.buttonMessage : '1X'}
+        </button>
+        <button onClick={() => {
+          this.redrawCanvas(2)
+        }}>
+            {this.props.buttonMessage ? this.props.buttonMessage : '2X'}
+        </button>
+      </div>
+    );
   }
 
   renderGrid() {
@@ -129,12 +146,12 @@ export default class ReactImageScaler extends React.Component {
 
   scaleImage(event) {
     const scale = event.target.value / 10;
-    this.scaleValueRef.current.value = scale;
     this.redrawCanvas(scale);
-    this.drawSourceCanvas(scale);
   }
 
   redrawCanvas(scale) {
+    this.scaleValueRef.current.value = scale;
+    this.drawSourceCanvas(scale);
     const ctx = this.canvas.getContext('2d');
     this.eraseCanvas(ctx);
 
@@ -168,7 +185,7 @@ export default class ReactImageScaler extends React.Component {
 
   returnData() {
     if(this.props.onScaleApply) {
-      this.processImageData();
+      this.props.onScaleApply(this.processImageData());
     }
     return null;
   }
@@ -180,12 +197,15 @@ export default class ReactImageScaler extends React.Component {
     // Get the scale of the current image.
     const imageWidth = this.image.width * scale;
     const imageHeight = this.image.height * scale;
+
+    const shouldCropWidth = imageWidth < this.canvas.width;
+    const shouldCropHeight = imageHeight < this.canvas.height;
     //Crop the scaled image.
-    const data = source.getImageData((this.sourceCanvas.width - imageWidth) / 2, (this.sourceCanvas.height - imageHeight) / 2, imageWidth, imageHeight)
+    const data = source.getImageData(shouldCropWidth ? (this.sourceCanvas.width - imageWidth) / 2 : 0, shouldCropHeight ? (this.sourceCanvas.height - imageHeight) / 2 : 0, shouldCropWidth ? imageWidth : this.canvas.width, shouldCropHeight ? imageHeight : this.canvas.height);
     //Set the canvas to the cropped with and height if needed to be cropped
-    imageWidth < this.canvas.width && (this.sourceCanvas.width = imageWidth);
-    imageHeight < this.canvas.height && (this.sourceCanvas.height = imageHeight);
-    console.log(this.sourceCanvas.width);
+    shouldCropWidth && (this.sourceCanvas.width = imageWidth);
+    shouldCropHeight && (this.sourceCanvas.height = imageHeight);
+
     return data;
   }
 
@@ -208,9 +228,10 @@ export default class ReactImageScaler extends React.Component {
     const cropped = this.determineCrop(sourceCtx, scale);
     sourceCtx.putImageData(cropped, 0, 0);
     const url = await this.sourceCanvas.toDataURL();
-    this.previewRef.current.src = url;
     // Reset the canvas to the enter size so we can grab the image
     this.sourceCanvas.width = this.state.canvasWidth;
     this.sourceCanvas.height = this.state.canvasHeight;
+
+    return url;
   }
 }
